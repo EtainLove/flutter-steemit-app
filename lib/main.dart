8dd88fde9b67a4c56902e13db301c9e8666d9eaf
안피곤 <markan82@gmail.com>
2018-10-26 00:59:14 +0900

    pull_to_refresh

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:steemlog/model/Post.dart';
 import 'package:steemlog/model/PostList.dart';
 import 'package:steemlog/network/SteemitRequest.dart';
 
+import "package:pull_to_refresh/pull_to_refresh.dart";
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -27,7 +29,14 @@ class FeedList extends StatefulWidget {
 
 class FeedListState extends State<FeedList> {
   final String _username = 'anpigon'; // 사용자 이름
-  final _limit = 20; // 가져올 피드 개수
+  final _limit = 5; // 가져올 피드 개수
+  RefreshController _refreshController;
+
+  @override
+  void initState() {
+    _refreshController = new RefreshController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,12 +48,82 @@ class FeedListState extends State<FeedList> {
     );
   }
 
+  void _onRefresh(bool up){
+    print('_onRefresh: ' + (up?'true':'false'));
+//    _refreshController.sendBack(true, RefreshStatus.idle);
+    if(up){
+      // 위로 당겼을때
+      SteemitRequest().fetchFeedList(_username, _limit).then((r) {
+        print('result:' + r.toString());
+        setState(() {});
+//        _refreshController.sendBack(false, RefreshStatus.idle);
+        _refreshController.sendBack(true, RefreshStatus.completed);
+      }).catchError(() {
+        _refreshController.sendBack(true, RefreshStatus.failed);
+      });
+      // 위로
+//      _refreshController.scrollTo(_refreshController.scrollController.offset+100.0);
+//      _refreshController.sendBack(true, RefreshStatus.idle);
+//      setState(() {});
+    }
+    else{
+      // 아래로 당겼을때
+      //footerIndicator Callback
+//      setState(() {});
+//      _refreshController.sendBack(false, RefreshStatus.idle);
+      SteemitRequest().fetchFeedList(_username, _limit).then((r) {
+        print('result:' + r.toString());
+        setState(() {});
+//        _refreshController.sendBack(false, RefreshStatus.idle);
+        _refreshController.sendBack(false, RefreshStatus.idle);
+      }).catchError(() {
+        _refreshController.sendBack(false, RefreshStatus.failed);
+      });
+    }
+  }
+
+  void _onOffsetCallback(bool isUp, double offset) {
+    // if you want change some widgets state ,you should rewrite the callback
+  }
+
+  Widget _headerCreate(BuildContext context, int mode) {
+    return new ClassicIndicator(mode: mode);
+  }
+
+  Widget _footerCreate(BuildContext context, int mode) {
+    return new ClassicIndicator(
+      mode: mode,
+      refreshingText: 'Loading...',
+//      idleIcon: const Icon(Icons.arrow_upward),
+      idleText: 'Loadmore...',
+    );
+  }
+
   Widget _buildSuggestions() {
     return new Center(
         child: FutureBuilder<PostList>(
             future: SteemitRequest().fetchFeedList(_username, _limit),
-            builder: (context, post) {
-              if (post.hasData) {
+            builder: (context, AsyncSnapshot<PostList> snapshot) {
+              print('snapshot.connectionState: ' + snapshot.connectionState.toString());
+              if (snapshot.hasData) {
+                return new SmartRefresher(
+                    enablePullUp: true,
+                    enablePullDown: true,
+                    controller: _refreshController,
+                    onRefresh: _onRefresh,
+//                    headerBuilder: _headerCreate,
+                    footerBuilder: _footerCreate,
+                    footerConfig: new RefreshConfig(),
+//                    onOffsetChange: _onOffsetCallback,
+                    child: new ListView.builder(
+                      padding: const EdgeInsets.all(5.0),
+                      itemCount: snapshot.data.result.length,
+                      itemBuilder: (BuildContext _context, int position) {
+                        return _buildRow(snapshot.data.result[position]);
+                      },
+                    )
+                );
+                /*
                 return new ListView.builder(
                   padding: const EdgeInsets.all(5.0),
                   itemCount: post.data.result.length,
@@ -52,8 +131,9 @@ class FeedListState extends State<FeedList> {
                     return _buildRow(post.data.result[position]);
                   },
                 );
-              } else if (post.hasError) {
-                return Text("${post.error}");
+                */
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
               }
               return CircularProgressIndicator();
             }
